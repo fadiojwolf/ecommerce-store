933f6baba9398d543d35d7266906f41564de0c15
Dmitri <dmitri.zgurski@iis.utm.md>
2025-09-18 16:13:37 +0300

    Add to Cart functionality

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Footer from '@/components/footer'
 import Navbar from '@/components/navbar'
 import Modal from '@/components/ui/modal'
 import ModalProvider from '@/providers/modul-provider'
+import ToastProvider from '@/providers/toast-provider'
 
 const font = Urbanist({ subsets: ['latin'] })
 
@@ -22,6 +23,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ModalProvider />
+        <ToastProvider />
         <Navbar />
         {children}
         <Footer />
--- components/navbar-actions.tsx
+++ components/navbar-actions.tsx
@@ -1,9 +1,11 @@
 "use client"
 
 import {ShoppingBag} from "lucide-react"
-import { useEffect, useState } from "react";
+import { use, useEffect, useState } from "react";
+import { useRouter } from "next/router";
 
 import Button from "./ui/button";
+import useCart from "@/hooks/use-cart";
 
 const NavbarAction = () => {
     const [isMounted, setIsMounted] = useState(false);
@@ -11,19 +13,23 @@ const NavbarAction = () => {
         setIsMounted(true);
     },[]);
 
+    const router = useRouter();
+    const cart = useCart();
+
+
     if(!isMounted) {
         return null;
     }
 
     return (
-        <div className="ml-auto flex items-center gap-x-4">
-            <Button className="flex item-center rounded-full bg-black px-4 py-2">
+        <div className="ml-auto flex item-center gap-x-4">
+            <Button onClick={() => router.push("/cart")} className="flex item-center rounded-full bg-black px-4 py-2">
                 <ShoppingBag
                     size={20}
                     color="white"
                 />
                 <span className="ml-2 text-sm font-medium text-white">
-                    0
+                    {cart.items.length}
                 </span>
             </Button>
         </div>
